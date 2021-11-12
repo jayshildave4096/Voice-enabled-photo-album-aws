11cbf963365644c1ce41d77e9ec54cd824112143
jayshildave4096 <davejayshil4096@gmail.com>
2021-11-12 17:51:54 -0500

    made changes to api

--- assets/js/photo.js
+++ assets/js/photo.js
@@ -1,9 +1,11 @@
 var checkout = {};
 var noteContent = "";
+var custom_tags = [];
+
 var searchquery = document.getElementById("search-query");
 try {
   var SpeechRecognition =
-	window.SpeechRecognition || window.webkitSpeechRecognition;
+    window.SpeechRecognition || window.webkitSpeechRecognition;
   var recognition = new SpeechRecognition();
 } catch (e) {
   console.error(e);
@@ -17,12 +19,12 @@ window.onload = function () {
   var searchtext = searchquery.value;
   var images = [];
   var params = {
-	"x-api-key": "WXvq5zPz9YkBLVPJx0aH8ECo6FDGEfH7FAHMqmS6",
-	q: searchtext,
+    "x-api-key": "WXvq5zPz9YkBLVPJx0aH8ECo6FDGEfH7FAHMqmS6",
+    q: searchtext,
   };
   sdk.searchGet(params, {}, {}).then(function (res) {
-	images = res.data;
-	generateArray(images);
+    images = res.data;
+    generateArray(images);
   });
 };
 
@@ -33,64 +35,66 @@ function generateArray(images) {
   num_columns = 4;
   k = 0;
   if (images.length <= 4) {
-	console.log("here");
-	var new_row = document.createElement("div");
-	new_row.className = "row";
-	for (i = 0; i < images.length; i++) {
-	  var col = document.createElement("div");
-	  col.className = "col-2";
-	  col.style = "padding:5px;";
-	  new_row.appendChild(col);
-	  var img = document.createElement("img");
-	  img.className = "img-thumbnail";
-	  img.setAttribute(
-		"src",
-		`https://hw2-ccbd-b2.s3.amazonaws.com/${images[i]}`
-	  );
-	  img.setAttribute("height", "200px");
-	  img.setAttribute("width", "200px");
-	  col.appendChild(img);
-	}
-	row.appendChild(new_row);
+    console.log("here");
+    var new_row = document.createElement("div");
+    new_row.className = "row";
+    for (i = 0; i < images.length; i++) {
+      var col = document.createElement("div");
+      col.className = "col-3";
+      col.style = "padding:5px;";
+      new_row.appendChild(col);
+      var img = document.createElement("img");
+      img.className = "img-thumbnail";
+      img.setAttribute(
+        "src",
+        `https://hw2-ccbd-b2.s3.amazonaws.com/${images[i]}`
+      );
+      img.setAttribute("height", "400px");
+      img.setAttribute("width", "400px");
+      col.appendChild(img);
+    }
+    row.appendChild(new_row);
   } else {
-	for (i = 0; i < num_rows; i++) {
-	  var new_row = document.createElement("div");
-	  new_row.className = "row";
-	  for (j = 0; j < 4; j++) {
-		var col = document.createElement("div");
-		col.className = "col-2";
-		col.style = "padding:5px;";
-		new_row.appendChild(col);
-		var img = document.createElement("img");
-		img.className = "img-thumbnail";
-		img.setAttribute(
-		  "src",
-		  `https://hw2-ccbd-b2.s3.amazonaws.com/${images[k]}`
-		);
-		img.setAttribute("height", "200px");
-		img.setAttribute("width", "200px");
-		col.appendChild(img);
-		k++;
-	  }
-	  row.appendChild(new_row);
-	}
+    var new_row=document.createElement("div");
+	new_row.className = "row";
+    for (i = 1; i <=images.length; i++) {
+      if (i % 3 == 0) {
+        new_row = document.createElement("div");
+        new_row.className = "row";
+	   } 
+        var col = document.createElement("div");
+        col.className = "col-2";
+        col.style = "padding:5px;";
+        new_row.appendChild(col);
+        var img = document.createElement("img");
+        img.className = "img-thumbnail";
+        img.setAttribute(
+          "src",
+          `https://hw2-ccbd-b2.s3.amazonaws.com/${images[i-1]}`
+        );
+        img.setAttribute("height", "200px");
+        img.setAttribute("width", "200px");
+        col.appendChild(img);
+        row.appendChild(new_row);
+      
+    }
   }
 }
 
 function searchImages() {
   var array = document.getElementById("image-array");
   while (array.firstChild) {
-	array.removeChild(array.firstChild);
+    array.removeChild(array.firstChild);
   }
   var searchtext = searchquery.value;
   var images = [];
   var params = {
-	"x-api-key": "WXvq5zPz9YkBLVPJx0aH8ECo6FDGEfH7FAHMqmS6",
-	q: searchtext,
+    "x-api-key": "WXvq5zPz9YkBLVPJx0aH8ECo6FDGEfH7FAHMqmS6",
+    q: searchtext,
   };
   sdk.searchGet(params, {}, {}).then(function (res) {
-	images = res.data;
-	generateArray(images);
+    images = res.data;
+    generateArray(images);
   });
 }
 
@@ -102,11 +106,12 @@ recognition.onresult = function (event) {
   var transcript = event.results[current][0].transcript;
 
   var mobileRepeatBug =
-	current == 1 && transcript == event.results[0][0].transcript;
+    current == 1 && transcript == event.results[0][0].transcript;
 
   if (!mobileRepeatBug) {
-	noteContent += transcript;
-	searchquery.value = noteContent;
+    noteContent += transcript;
+    searchquery.value = noteContent;
+	noteContent="";
   }
 };
 
@@ -116,75 +121,59 @@ recognition.onstart = function () {
 
 recognition.onspeechend = function () {
   console.log(
-	"You were quiet for a while so voice recognition turned itself off."
+    "You were quiet for a while so voice recognition turned itself off."
   );
 };
 
 recognition.onerror = function (event) {
   if (event.error == "no-speech") {
-	console.log("No speech was detected. Try again.");
+    console.log("No speech was detected. Try again.");
   }
 };
 
 //HELPER FUNCTIONS
-// function uploadPhoto() {
-//     let imgFile = document.getElementById("file_path").files[0]
-//     reader = new FileReader()
-//     reader.onload = function(e) {
-//       console.log("Here")
-//       var params = {
-//         "x-api-key": "WnqHXLguUU3sf7qGMjmOLaRCD3gmg89J7VyE8qvG",
-//         "file-name": imgFile.name,
-//         "Content-Type": imgFile.type,
-//         "bucket": "nm3223.voice-photo-album.b2.w6998.ccbd.f2021",
-//         "x-amz-meta-customLabels": document.getElementById("uploadText").value
-//       }
-//       var apigClient = apigClientFactory.newClient({
-//         "apiKey": "WnqHXLguUU3sf7qGMjmOLaRCD3gmg89J7VyE8qvG"
-//       })
-//       apigClient.uploadPut(params, Uint8Array(e.target.result),{}).then(function(res) {
-//         console.log(res.status)
-//         if(res.status == 200) {
-//           document.getElementById("fileName").innerHTML = "File Upload Complete"
-//         }
-//       })
-
-//     }
-//     reader.readAsArrayBuffer(imgFile)
-//   }
 
 function uploadImage() {
-  const file = document.querySelector("input[type=file]").files[0];
+  var file = document.getElementById("file").files[0];
   const reader = new FileReader();
-  reader.addEventListener(
-	"load",
-	function () {
-	  body = reader.result;
-	  var bytes = [];
-	  var bytesv2 = [];
 
-	  for (var i = 0; i < body.length; ++i) {
-		var code = body.charCodeAt(i);
+  var encoded_image = getBase64(file).then((data) => {
+    console.log(file.name);
+    var body = data;
+    var params = {
+      "x-api-key": "WXvq5zPz9YkBLVPJx0aH8ECo6FDGEfH7FAHMqmS6",
+      "file-name": file.name,
+      "Content-Type": file.type + ";base64",
+      bucket: "hw2-ccbd-b2",
+      "x-amz-meta-customLabels": "",
+    };
+    var additionalParams = {};
+    sdk.uploadPut(params, body, additionalParams).then(function (res) {
+      console.log(res);
+      if (res.status == 200) {
+        alert("File Uploaded Successfully");
+      }
+      searchquery.value = "";
+      // searchImages();
+      window.location.reload();
+    });
+  });
+}
 
-		bytes = bytes.concat([code]);
-
-		bytesv2 = bytesv2.concat([code & 0xff, (code / 256) >>> 0]);
-	  }
-	  sdk
-		.uploadPut({}, bytes, {})
-		.then((response) => {
-		  console.log(response);
-		})
-		.catch((error) => {
-		  alert("an error occurred", error);
-		});
-	},
-	false
-  );
-
-  if (file) {
-	reader.readAsDataURL(file);
-  }
+function getBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    // reader.onload = () => resolve(reader.result)
+    reader.onload = () => {
+      let encoded = reader.result.replace(/^data:(.*;base64,)?/, "");
+      if (encoded.length % 4 > 0) {
+        encoded += "=".repeat(4 - (encoded.length % 4));
+      }
+      resolve(encoded);
+    };
+    reader.onerror = (error) => reject(error);
+  });
 }
 
 function insert_tags() {
@@ -192,17 +181,18 @@ function insert_tags() {
   var ul = document.getElementById("input-tag-list");
 
   if (input.value != "") {
-	var li = document.createElement("li");
-	li.className = "list-group-item";
-	li.textContent = input.value;
-	ul.appendChild(li);
+    var li = document.createElement("li");
+    li.className = "list-group-item";
+    li.textContent = input.value;
+    custom_tags.push(input.value);
+    ul.appendChild(li);
   }
 }
 
 function startRecording() {
   searchquery.value = "";
   if (noteContent.length) {
-	noteContent += " ";
+    noteContent += " ";
   }
   recognition.start();
   console.log("Voice recognition started.");
@@ -214,6 +204,7 @@ function changeText() {
 
 function stopRecording() {
   recognition.stop();
+  searchquery.value = "";
   console.log("Voice recognition paused.");
 }
 
